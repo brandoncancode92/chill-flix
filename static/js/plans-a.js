b70ec4149767b4927e51882f9bfb4bc2d4ed17de
Brandon Colangelo <brandoncancode@gmail.com>
2023-11-07 13:51:07 -0500

    Added onhover events to the plans-a HTML and plans-a.js. Also changed the sign-in HTML backround property.

---
+++ static/js/plans-a.js
@@ -0,0 +1,31 @@
+const boxOne = document.getElementById('box-one');
+const boxTwo = document.getElementById('box-two');
+const boxThree = document.getElementById('box-three');
+
+const standardAdsText = document.querySelectorAll('.standard-ads');
+const standardText = document.querySelectorAll('.standard');
+const premiumText = document.querySelectorAll('.premium');
+
+boxOne.addEventListener('mouseover', () => {
+  standardAdsText.forEach(element => element.style.color = 'red');
+});
+
+boxTwo.addEventListener('mouseover', () => {
+  standardText.forEach(element => element.style.color = 'red');
+});
+
+boxThree.addEventListener('mouseover', () => {
+  premiumText.forEach(element => element.style.color = 'red');
+});
+
+boxOne.addEventListener('mouseout', () => {
+  standardAdsText.forEach(element => element.style.color = '');
+});
+
+boxTwo.addEventListener('mouseout', () => {
+  standardText.forEach(element => element.style.color = '');
+});
+
+boxThree.addEventListener('mouseout', () => {
+  premiumText.forEach(element => element.style.color = '');
+});
