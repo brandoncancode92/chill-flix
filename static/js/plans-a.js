aa7ee10ef1c2c1e984214fe1b0ebf70e6149162c
Brandon Colangelo <brandoncancode@gmail.com>
2023-11-07 15:16:48 -0500

    Documentation to index HTML.

--- static/js/plans-a.js
+++ static/js/plans-a.js
@@ -7,25 +7,25 @@ const standardText = document.querySelectorAll('.standard');
 const premiumText = document.querySelectorAll('.premium');
 
 boxOne.addEventListener('mouseover', () => {
-  standardAdsText.forEach(element => element.style.color = 'red');
+    standardAdsText.forEach(element => element.style.color = 'red');
 });
 
 boxTwo.addEventListener('mouseover', () => {
-  standardText.forEach(element => element.style.color = 'red');
+    standardText.forEach(element => element.style.color = 'red');
 });
 
 boxThree.addEventListener('mouseover', () => {
-  premiumText.forEach(element => element.style.color = 'red');
+    premiumText.forEach(element => element.style.color = 'red');
 });
 
 boxOne.addEventListener('mouseout', () => {
-  standardAdsText.forEach(element => element.style.color = '');
+    standardAdsText.forEach(element => element.style.color = '');
 });
 
 boxTwo.addEventListener('mouseout', () => {
-  standardText.forEach(element => element.style.color = '');
+    standardText.forEach(element => element.style.color = '');
 });
 
 boxThree.addEventListener('mouseout', () => {
-  premiumText.forEach(element => element.style.color = '');
+    premiumText.forEach(element => element.style.color = '');
 });
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-// Get the divs with the ids `faq-1` and `faq-1-info`.
+// Get the divs with the correct ids and div-info ids.
 const faq1Div = document.getElementById('faq-1');
 const faq1InfoDiv = document.getElementById('faq-1-info');
 
@@ -23,39 +23,57 @@ faq1Div.addEventListener('click', function() {
     faq1InfoDiv.style.display = faq1InfoDiv.style.display === 'none' ? 'block' : 'none';
 });
 
-// Add an event listener to the `faq-1` div to listen for click events.
+// Add an event listener to the `faq-2` div to listen for click events.
 faq2Div.addEventListener('click', function() {
-  // Toggle the `display` property of the `faq-1-info` div.
+  // Toggle the `display` property of the `faq-2-info` div.
     faq2InfoDiv.style.display = faq2InfoDiv.style.display === 'none' ? 'block' : 'none';
 });
 
-// Add an event listener to the `faq-1` div to listen for click events.
+// Add an event listener to the `faq-3` div to listen for click events.
 faq3Div.addEventListener('click', function() {
-  // Toggle the `display` property of the `faq-1-info` div.
+  // Toggle the `display` property of the `faq-3-info` div.
     faq3InfoDiv.style.display = faq3InfoDiv.style.display === 'none' ? 'block' : 'none';
 });
 
-// Add an event listener to the `faq-1` div to listen for click events.
+// Add an event listener to the `faq-4` div to listen for click events.
 faq4Div.addEventListener('click', function() {
-  // Toggle the `display` property of the `faq-1-info` div.
+  // Toggle the `display` property of the `faq-4-info` div.
     faq4InfoDiv.style.display = faq4InfoDiv.style.display === 'none' ? 'block' : 'none';
 });
 
-// Add an event listener to the `faq-1` div to listen for click events.
+// Add an event listener to the `faq-5` div to listen for click events.
 faq5Div.addEventListener('click', function() {
-  // Toggle the `display` property of the `faq-1-info` div.
+  // Toggle the `display` property of the `faq-5-info` div.
     faq5InfoDiv.style.display = faq5InfoDiv.style.display === 'none' ? 'block' : 'none';
 });
 
-// Add an event listener to the `faq-1` div to listen for click events.
+// Add an event listener to the `faq-6` div to listen for click events.
 faq6Div.addEventListener('click', function() {
-  // Toggle the `display` property of the `faq-1-info` div.
+  // Toggle the `display` property of the `faq-6-info` div.
     faq6InfoDiv.style.display = faq6InfoDiv.style.display === 'none' ? 'block' : 'none';
 });
 
-const faq1Divplus = document.getElementById('faq-1');
-const plusElement = faq1Div.querySelector('.plus');
+// const faq1Divplus = document.getElementById('faq-1');
+// const plusElement = faq1Div.querySelector('.plus');
 
-faq1Div.addEventListener('click', function() {
-  plusElement.style.transform = plusElement.style.transform === 'rotate(45deg)' ? 'rotate(0deg)' : 'rotate(45deg)';
+// faq1Div.addEventListener('click', function() {
+//   plusElement.style.transform = plusElement.style.transform === 'rotate(45deg)' ? 'rotate(0deg)' : 'rotate(45deg)';
+// });
+
+// To rotate the plus signs on click
+const faqElements = document.querySelectorAll('.gray.mb-2.p-3.flex-column');
+
+faqElements.forEach(faqElement => {
+    const plusH3 = faqElement.querySelector('h3[id^=plus]');
+    const faqInfoElement = faqElement.querySelector('.faq-info');
+
+    faqElement.addEventListener('click', () => {
+        if (plusH3.style.transform === 'rotate(-45deg)') {
+            plusH3.style.transform = 'rotate(0deg)';
+            faqInfoElement.classList.add('hidden');
+        } else {
+            plusH3.style.transform = 'rotate(-45deg)';
+            faqInfoElement.classList.remove('hidden');
+        }
+    });
 });
